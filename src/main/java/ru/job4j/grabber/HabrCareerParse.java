9288c4056b2559307faae282f66ffeab73a823d4
bogdanove <anoth3rx@gmail.com>
2022-08-30 19:44:15 +0300

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -16,32 +16,43 @@ public class HabrCareerParse implements Parse {
 
     private final DateTimeParser dateTimeParser;
 
+    private static final int PAGE = 5;
+
     public HabrCareerParse(DateTimeParser dateTimeParser) {
         this.dateTimeParser = dateTimeParser;
     }
 
     private static String retrieveDescription(String link) throws IOException {
-        Connection connection = Jsoup.connect(String.format(link));
-        Document document = connection.get();
-        Elements rows = document.select(".collapsible-description__content");
-        return rows.get(0).child(0).text();
+        String desc = null;
+        try {
+            Connection connection = Jsoup.connect(String.format(link));
+            Document document = connection.get();
+            Elements rows = document.select(".collapsible-description__content");
+            desc = rows.get(0).child(0).text();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return desc;
     }
 
     @Override
     public List<Post> list(String link) throws IOException {
+        if (!link.endsWith("?page=")) {
+            throw new IllegalArgumentException(String.format("Invalid link %s!", link));
+        }
         List<Post> posts = new ArrayList<>();
-        for (int i = 1; i < 6; i++) {
-            Connection connection = Jsoup.connect(String.format("%s?page=%s", link, i));
+        for (int i = 1; i <= PAGE; i++) {
+            Connection connection = Jsoup.connect(String.format("%s%s", link, i));
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
             rows.forEach(row -> {
-                Element titleElement = row.select(".vacancy-card__title").first();
-                Element linkElement = titleElement.child(0);
-                String vacancyName = titleElement.text();
-                String href = String.format("%s%s", link, linkElement.attr("href"));
-                Element dateElement = row.select(".vacancy-card__date").first().child(0);
-                String dateVacancy = dateElement.attr("datetime");
                 try {
+                    Element titleElement = row.select(".vacancy-card__title").first();
+                    Element linkElement = titleElement.child(0);
+                    String vacancyName = titleElement.text();
+                    String href = String.format("%s%s", link, linkElement.attr("href"));
+                    Element dateElement = row.select(".vacancy-card__date").first().child(0);
+                    String dateVacancy = dateElement.attr("datetime");
                     posts.add(new Post(vacancyName, href, retrieveDescription(href), dateTimeParser.parse(dateVacancy)));
                 } catch (Exception e) {
                     e.printStackTrace();
