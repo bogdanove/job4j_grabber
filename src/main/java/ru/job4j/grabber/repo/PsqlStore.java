0357dafaf4a12b72155aa49b4af1e790551bedcd
bogdanove <anoth3rx@gmail.com>
2022-08-31 22:03:35 +0300

    5. PsqlStore [#285209]

---
+++ src/main/java/ru/job4j/grabber/repo/PsqlStore.java
@@ -0,0 +1,105 @@
+package ru.job4j.grabber.repo;
+
+import ru.job4j.grabber.model.Post;
+import ru.job4j.quartz.AlertRabbit;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store, AutoCloseable {
+
+    private Connection cn;
+
+    public PsqlStore(Properties cfg) {
+        try {
+            Class.forName(cfg.getProperty("driver-class-name"));
+            cn = DriverManager.getConnection(
+                    cfg.getProperty("url"),
+                    cfg.getProperty("username"),
+                    cfg.getProperty("password"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement ps = cn.prepareStatement("insert into post(name, text, link, created) values (?, ?, ?, ?)")) {
+            ps.setString(1, post.getTitle());
+            ps.setString(2, post.getDescription());
+            ps.setString(3, post.getLink());
+            ps.setTimestamp(4, new Timestamp(System.currentTimeMillis()));
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> posts = new ArrayList<>();
+        try (PreparedStatement ps = cn.prepareStatement("select * from post;")) {
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    posts.add(postMapper(rs));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = null;
+        try (PreparedStatement ps = cn.prepareStatement("select * from post where id = ?")) {
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    post = postMapper(rs);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cn != null) {
+            cn.close();
+        }
+    }
+
+    private Post postMapper(ResultSet rs) throws SQLException {
+        return new Post(
+                rs.getInt("id"),
+                rs.getString("name"),
+                rs.getString("link"),
+                rs.getString("text"),
+                rs.getTimestamp("created").toLocalDateTime()
+        );
+    }
+
+    public static void main(String[] args) {
+        Properties prop = new Properties();
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            prop.load(in);
+            PsqlStore store = new PsqlStore(prop);
+            store.save(new Post("vacancy name", "vacancy link", "vacancy description", LocalDateTime.now()));
+            store.save(new Post("vacancy name2", "vacancy link2", "vacancy description2", LocalDateTime.now()));
+            System.out.println(store.findById(2));
+            System.out.println(store.getAll());
+            store.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
